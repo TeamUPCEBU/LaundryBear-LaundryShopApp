f687a05b0d0ac5caad7833e8d87506e82a392ac2
Ember Sabsalon <embersabsalon@gmail.com>
2016-07-16 15:12:40 +0800

    Create new app

---
+++ app/build/generated/source/buildConfig/androidTest/debug/laundrybear/laundrybear_laundryshopapp/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package laundrybear.laundrybear_laundryshopapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "laundrybear.laundrybear_laundryshopapp.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
