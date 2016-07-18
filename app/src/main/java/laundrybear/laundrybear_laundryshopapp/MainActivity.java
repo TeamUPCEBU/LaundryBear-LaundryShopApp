bf27c79ce11fbd7cb5a1f182c7ee74b198b0623a
Ember Sabsalon <embersabsalon@gmail.com>
2016-07-18 17:08:18 +0800

    Add back functionality

--- app/src/main/java/laundrybear/laundrybear_laundryshopapp/MainActivity.java
+++ app/src/main/java/laundrybear/laundrybear_laundryshopapp/MainActivity.java
@@ -2,19 +2,36 @@ package laundrybear.laundrybear_laundryshopapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    static WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webview = new WebView(this);
+        webview = new WebView(this);
         setContentView(webview);
         webview.setWebViewClient(new WebViewClient());
-        webview.loadUrl("http://10.48.32.48:8000/login?next=/");
+        webview.loadUrl("http://google.com")
 
     }
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webview.canGoBack()) {
+                        webview.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
