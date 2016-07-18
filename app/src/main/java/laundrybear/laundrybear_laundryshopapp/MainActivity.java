d75cb05369d47b16712d6242105ab6b5b0d819d1
Ember Sabsalon <embersabsalon@gmail.com>
2016-07-18 15:31:47 +0800

    Add site

--- app/src/main/java/laundrybear/laundrybear_laundryshopapp/MainActivity.java
+++ app/src/main/java/laundrybear/laundrybear_laundryshopapp/MainActivity.java
@@ -1,7 +1,9 @@
 package laundrybear.laundrybear_laundryshopapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://10.48.32.48:8000/login?next=/");
+
     }
 }
